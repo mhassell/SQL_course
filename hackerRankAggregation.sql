90219b30ecb246efcc1d21bcd31729adafeb8514
Matthew Hassell <hassell.matthew@gmail.com>
2017-08-07 06:46:01 -0400

    some work on sunday

--- hackerRankAggregation.sql
+++ hackerRankAggregation.sql
@@ -22,3 +22,32 @@ Select ceil(avg(salary) - avg(cast(replace(cast(salary as char(10)), '0', '') as
 
 --I need to understand this one better
 select max(months*salary), count(*) from employee where salary*months = (select max(months*salary) from employee)
+
+-- Weather Observation Station 12
+select round(sum(lat_n),2), round(sum(long_w),2) from station
+
+-- Weather Observation Station 13
+select round(sum(lat_n),4) from station where (lat_n > 38.7880 and lat_n < 137.2345)
+
+-- Weather Observation Station 13
+select truncate(max(lat_n),4) from station where lat_n < 137.2345
+
+-- Weather Observation Station 15
+select round(long_w,4) from station where lat_n < 137.2345
+order by lat_n desc
+limit 1
+
+-- Weather Observation Station 16
+select round(lat_n, 4) from station where lat_n > 38.7780 order by lat_n asc limit 1
+
+-- Weather Observation Station 17
+select round(long_w, 4) from station where lat_n > 38.7780 order by lat_n asc limit 1
+
+-- Weather Observation Station 18
+select round(abs(min(lat_n)-max(lat_n)) + abs(min(long_w)-max(long_w)),4) from station  
+
+-- Weather Observation Station 19
+select round(sqrt(power(min(lat_n)-max(lat_n),2)+power(min(long_w)-max(long_w),2)),4) from station
+
+-- Weather Observation Station 20
+select round(lat_n,4) from station order by lat_n asc limit 1 offset 249
--- hackerRankBasicJoin.sql
+++ hackerRankBasicJoin.sql
@@ -1,15 +1,18 @@
-# Asian Population
+-- Asian Population
 SELECT SUM(City.Population) FROM CITY INNER JOIN COUNTRY ON 
 City.CountryCode = Country.Code WHERE Country.Continent = "Asia"
 
-# African Cities
+-- African Cities
 SELECT City.Name from CITY INNER JOIN COUNTRY ON 
 City.CountryCode = Country.Code WHERE Country.Continent = "Africa"
 
-# Average Population of Each Continent
+-- Average Population of Each Continent
 SELECT Country.Continent, FLOOR(AVG(City.Population)) FROM CITY INNER JOIN COUNTRY ON
 City.CountryCode = Country.Code GROUP BY Country.Continent
 
-# The Report
+-- The Report
 SELECT IF(GRADE >= 8, SELECT STUDENTS.NAME, GRADES.Grade, STUDENTS.Marks FROM STUDENTS INNER JOIN ON
 		STUDENTS.MARK >= GRADES.Min_Mark AND STUDENTS.MARK <= MAX_MARK)
+
+-- Top Competitors
+select hacker_id, name from 
