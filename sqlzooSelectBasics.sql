17a7da7cb03278cc887ae377d924de551e9c44cf
Matthew Hassell <hassell.matthew@gmail.com>
2017-08-08 19:26:22 -0400

    some basics

--- sqlzooSelectBasics.sql
+++ sqlzooSelectBasics.sql
@@ -8,3 +8,20 @@ SELECT name, population FROM world
 
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+SELECT name, continent, population FROM world
+
+SELECT name FROM world
+WHERE population > 200000000
+
+select name, gdp/population from world where population > 200000000
+
+select name, population/1000000 from world where continent = 'south america'
+
+select name, population from world where name in ('France', 'Germany', 'Italy')
+
+select name from world where name like '%united%'
+
+select name, population, area from world where (population > 250000000 or area > 3000000)
+
+select name, population, area from world where (population > 250000000 xor area > 3000000)
