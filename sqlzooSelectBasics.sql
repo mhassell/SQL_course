0b5994264ca6dff02c9a3295a20625d3fdb8f52e
Matthew Hassell <hassell.matthew@gmail.com>
2017-08-09 19:11:31 -0400

    so tired

--- sqlzooSelectBasics.sql
+++ sqlzooSelectBasics.sql
@@ -25,3 +25,13 @@ select name from world where name like '%united%'
 select name, population, area from world where (population > 250000000 or area > 3000000)
 
 select name, population, area from world where (population > 250000000 xor area > 3000000)
+
+select name, round(population/1000000,2), round(gdp/1000000000,2)  from world where continent = 'south america'
+
+select name, round(gdp/population, -3) from world where gdp > 1000000000000
+
+SELECT name, capital FROM world where ( LEFT(name,1) = left(capital,1) and not (capital = name) )
+
+SELECT name FROM world
+WHERE name LIKE '%a%' and name LIKE '%e%' and  name LIKE '%i%' and  name LIKE '%o%'
+and  name LIKE '%u%' AND name NOT LIKE '% %'
