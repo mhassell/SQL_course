630bb67be5859260b35c277b79103231c90f33f8
Matthew Hassell <hassell.matthew@gmail.com>
2017-08-13 08:50:48 -0400

    some weekend practice

--- sqlzooSelectWithinSelect.sql
+++ sqlzooSelectWithinSelect.sql
@@ -11,5 +11,9 @@ select name, concat(round(100*population/(select population from world where nam
 select name from world where gdp > all(select gdp from world where continent = 'Europe' and gdp >0)
 
 SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y
-WHERE y.continent=x.continent
-AND area>0)
+WHERE y.continent=x.continent AND area>0)
+
+select continent, name from world x where x.name = (select name from world y where x.continent = y.continent order by name asc limit 1)
+
+select name, continent, population from world x where name in (select name from world y where all(y.population <= 25000000))
+
