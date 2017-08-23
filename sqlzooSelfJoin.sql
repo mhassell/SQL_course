579cc5723b1936eff9c013c03d1fd3e3a1dfbc99
Matthew Hassell <hassell.matthew@gmail.com>
2017-08-23 18:30:02 -0400

    all but done

--- sqlzooSelfJoin.sql
+++ sqlzooSelfJoin.sql
@@ -11,3 +11,37 @@ SELECT company, num, COUNT(num)
 	FROM route WHERE stop=149 OR stop=53
 	GROUP BY company, num
 	HAVING count(num) = 2
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  WHERE a.stop=53 AND b.stop = 149
+
+-- why is this necessary??
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  WHERE a.stop=115 AND b.stop = 137
+
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross'
+
+SELECT distinct stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+
+
